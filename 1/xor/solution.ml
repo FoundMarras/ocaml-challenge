3e4ddee49f0335619227f4b727d39546c415f912
FoundMarras <olinadsarram999@gmail.com>
2024-10-11 21:03:21 +0200

    Soluzione esercizio 1/xor

---
+++ 1/xor/solution.ml
@@ -0,0 +1,35 @@
+(*
+XOR:
+  false false -> false
+  false true -> true 
+  true false -> true
+  true true -> false
+  *)
+
+let xor1 : bool -> bool -> bool =
+  fun a b ->
+    (not a && b) || (a && not b)
+;;
+
+let xor2 : bool -> bool -> bool =
+  fun a b ->
+    if a
+      then(
+        if b 
+          then false
+        else true
+      )
+        else(
+          if b
+            then true
+          else false
+        )
+;;
+
+let xor3 : bool -> bool -> bool =
+  fun a b ->
+    match (a,b) with
+    | (false,true) -> true
+    | (true,false) -> true
+    | _ -> false
+  ;;
