79d937e284c6f0aa7080466dc94c036d64592389
FoundMarras <olinadsarram999@gmail.com>
2024-10-12 12:29:49 +0200

    Soluzione esercizio 1/mux

--- 1/mux/solution.ml
+++ 1/mux/solution.ml
@@ -28,3 +28,26 @@ let mux2P (s0:bool) (a:bool) (b:bool) : bool =
 ;;
 
 
+let mux4 (s0:bool) (s1:bool) (a0:bool) (a1:bool) (a2:bool) (a3:bool): bool =
+  match (s0,s1) with
+  | (false,false) -> a0
+  | (false,true) -> a1
+  | (true,false) -> a2
+  | (true,true) -> a3
+;;
+
+let mux4R (s0:bool) (s1:bool) (a0:bool) (a1:bool) (a2:bool) (a3:bool): bool =
+  if (mux2C s0 true false)
+    then mux2C s1 a3 a2
+    else mux2C s1 a1 a0
+;;
+
+assert(mux4 false false false true false true = false);;
+assert(mux4 false true false true false true = true);;
+assert(mux4 true false false true false true = false);;
+assert(mux4 true true false true false true = true);;
+
+assert(mux4R false false false true false true = false);;
+assert(mux4R false true false true false true = true);;
+assert(mux4R true false false true false true = false);;
+assert(mux4R true true false true false true = true);;
