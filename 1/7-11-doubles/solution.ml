7087ea086f4f547625ed345cbe561e5aef480a99
FoundMarras <olinadsarram999@gmail.com>
2024-10-20 19:42:22 +0200

    Soluzione esercizio 1/7-11-doubles

---
+++ 1/7-11-doubles/solution.ml
@@ -0,0 +1,8 @@
+let seven_eleven_doubles : unit -> bool * int * int =
+  fun ()->
+    let d1 = Random.int(6) + 1 in
+    let d2 = Random.int(6) + 1 in
+    if ((d1 + d2) = 7 || (d1 + d2) = 11 || (d1= d2))
+      then (true,d1,d2) 
+      else (false,d1,d2)
+  ;;
